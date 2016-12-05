cffa93271399582b001fe9840e83fa020dc030fe
Max <maxgomesrj@gmail.com>
2016-12-05 18:24:05 -0300

    Imprimir valores entre 150 a 300

---
+++ Numero_de_150_a_300.java
@@ -0,0 +1,14 @@
+
+public class Numero_de_150_a_300 {
+	public  static void main(String [] args){
+
+		
+		//numero++;
+		for(int numero = 0; numero <= 300; numero++){
+			if (numero >= 150 && numero <= 300)
+		System.out.println(numero);
+		}
+		
+	}
+
+}
